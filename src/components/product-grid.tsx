233ccda42f83cbeaa72d0dbc3dfe8e5671ab47a0
Eddie Shin <eshinhw@gmail.com>
2023-10-11 21:34:03 -0400

    Apply zustand code

--- src/components/product-grid.tsx
+++ src/components/product-grid.tsx
@@ -1,3 +1,5 @@
+"use client";
+import useCart from "@/app/store";
 import Link from "next/link";
 import React from "react";
 
@@ -5,8 +7,18 @@ export default function ProductGrid(props: any) {
   const { product } = props;
   const { unit_amount: cost, product: productInfo } = product;
   const { id: productId, name: name } = productInfo;
+  const setProduct = useCart((state) => state.setProduct);
+  const onProductClick = () => {
+    const newProduct = {
+      name,
+      productId,
+      cost,
+      productInfo,
+    };
+    setProduct({ newProduct });
+  };
   return (
-    <Link href={`/product/${productId}`}>
+    <Link href={`/product/${productId}`} onClick={onProductClick}>
       <div className="flex flex-col shadow bg-white hover:shadow-xl cursor-pointer">
         <img
           className="w-full h-full object-cover"
@@ -20,46 +32,3 @@ export default function ProductGrid(props: any) {
     </Link>
   );
 }
-
-// {
-//   id: 'price_1Nz6LlL9XXpuYWUZJkgYocaQ',
-//   object: 'price',
-//   active: true,
-//   billing_scheme: 'per_unit',
-//   created: 1696807529,
-//   currency: 'cad',
-//   custom_unit_amount: null,
-//   livemode: false,
-//   lookup_key: null,
-//   metadata: {},
-//   nickname: null,
-//   product: {
-//     id: 'prod_OmfhrEn68mT403',
-//     object: 'product',
-//     active: true,
-//     attributes: [],
-//     created: 1696807529,
-//     default_price: 'price_1Nz6LlL9XXpuYWUZJkgYocaQ',
-//     description: null,
-//     features: [],
-//     images: [Array],
-//     livemode: false,
-//     metadata: {},
-//     name: 'Banana',
-//     package_dimensions: null,
-//     shippable: null,
-//     statement_descriptor: null,
-//     tax_code: null,
-//     type: 'service',
-//     unit_label: null,
-//     updated: 1696807530,
-//     url: null
-//   },
-//   recurring: null,
-//   tax_behavior: 'unspecified',
-//   tiers_mode: null,
-//   transform_quantity: null,
-//   type: 'one_time',
-//   unit_amount: 2500,
-//   unit_amount_decimal: '2500'
-// }
