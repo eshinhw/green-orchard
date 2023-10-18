3ea9d8dc4b6686466fb301da2871d3de68102d02
Eddie Shin <eshinhw@gmail.com>
2023-10-18 14:36:10 -0400

    Create CartPage

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function CartPage() {
+  return (
+    <>
+      <h1 className="text-center my-5 text-2xl">Your Cart</h1>
+      <p>Your cart is currently empty.</p>
+      <p>Continue browsing here.</p>
+    </>
+  );
+}
