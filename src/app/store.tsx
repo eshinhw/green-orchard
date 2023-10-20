cdea8d5bb1c706f09fb77b3812942d3a7876c3f5
Eddie Shin <eshinhw@gmail.com>
2023-10-19 23:26:11 -0400

    Update types

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -3,38 +3,50 @@ import { create } from "zustand";
 const useCart = create((set, get) => ({
   cart: [], // list of all the products and quantities
   product: {}, // the product which is selected
-  setProduct: (params) => {
+  setProduct: (params: any) => {
     const { newProduct } = params;
-    set((state) => {
+    set((state: any) => {
       return { ...state, product: newProduct };
     });
   },
   openModal: false,
   setOpenModal: () => {
-    set((state) => {
+    set((state: any) => {
       return { ...state, openModal: !state.openModal };
     });
   },
-  addItem: (params) => {
+  addItem: (params: any) => {
     const { newItem } = params;
-
-    set((state) => {
-      const newCart = [...state.cart, newItem];
-      return { ...state, cart: newCart };
-    }); // set method receives the current state and return the new state
+    console.log(newItem);
+    set((state: any) => {
+      // Check if newItem already exists in cart
+      const foundIdx = state.cart.findIndex(
+        (item: any) => item.id === newItem.id
+      );
+      if (foundIdx === -1) {
+        const newCart = [...state.cart, newItem];
+        return { ...state, cart: newCart };
+      } else {
+        state.cart[foundIdx].quantity =
+          state.cart[foundIdx].quantity + newItem.quantity;
+        return { ...state };
+      }
+    });
   },
-  removeItem: (params) => {
+  removeItem: (params: any) => {
     const { itemIndex } = params;
-    set((state) => {
-      const newCart = state.cart.filter((element, elementIndex) => {
-        return elementIndex !== itemIndex;
-      });
+    set((state: any) => {
+      const newCart = state.cart.filter(
+        (element: any, elementIndex: number) => {
+          return elementIndex !== itemIndex;
+        }
+      );
       return { ...state, cart: newCart };
     });
   },
   emptyCart: () => {
     set((state: any) => {
-      const newCart = [];
+      const newCart: any = [];
       return { ...state, cart: newCart };
     });
   },
