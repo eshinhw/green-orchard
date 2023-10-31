edfac24ea37dc4bbd57f3643ff29f4086ad15204
Eddie Shin <eshinhw@gmail.com>
2023-10-30 22:44:00 -0400

    Update header style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 p-6 bg-white border-b border-solid border-lime-800 shadow-lg z-50 text-black text-2xl sm:text-3xl md:text-4xl sm:p-8 flex item-center justify-between">
       <Link href={"/"}>
-        <h1 className="uppercase cursor-pointer hover:scale-110">
+        <h1 className="uppercase cursor-pointer hover:scale-110 text-green-600">
           Green Orchard
         </h1>
       </Link>
